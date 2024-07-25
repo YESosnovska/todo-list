97a852bb9d9c01551a8f51ad5f4c4a37205e5059
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-07-25 15:47:52 +0300

    create TaskCreateView, index.html and task-create.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from models import Task
+from tasks.models import Task
 
 
 class TaskListView(generic.ListView):
     model = Task
     context_object_name = "task_list"
+    template_name = "tasks/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
 
