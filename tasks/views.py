e744b0202e55d6a0fad463e6a39a57961e969108
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-07-25 17:27:41 +0300

    make Task tags choice field more user-friendly

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 
+from tasks.forms import TaskForm
 from tasks.models import Task, Tag
 
 
@@ -13,14 +14,14 @@ class TaskListView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
     success_url = reverse_lazy("tasks:index")
+    form_class = TaskForm
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = "__all__"
     success_url = reverse_lazy("tasks:index")
+    form_class = TaskForm
 
 
 class TaskDeleteView(generic.DeleteView):
