c3841beda7a377f4f09780969624dd9b87c01f5b
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-07-25 16:03:12 +0300

    fix settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
+    "crispy_bootstrap4",
     "tasks"
 ]
 
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
