c98856ce767116a5e281dc182a471cac42a6d514
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-07-25 15:17:03 +0300

    implement TaskListView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-urlpatterns = [
+from tasks.views import TaskListView
 
+urlpatterns = [
+    path("", TaskListView.as_view(), name="index"),
 ]
 
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+
